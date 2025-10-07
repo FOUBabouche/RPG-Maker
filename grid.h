15a0f9b196ff01370508902aa3bf901ccf4b5eb5
FOUBabouche <52860373+FOUBabouche@users.noreply.github.com>
2025-10-07 17:28:36 +0200

    Update grid.h

--- grid.h
+++ grid.h
@@ -5,4 +5,15 @@
 #define SFML_STATIC
 #endif
 
+#include <SFML/System/Vector2.hpp>
+
+class Grid {
+public:
+	Grid() = default;
+	Grid(sf::Vector2f size, sf::Vector2f tileSize);
+
+	void setTile(sf::Vector2f position);
+	
+};
+
 #endif
