d14b6a2fc5b7e0c8b6d67bdb8f5cdfd05e8cd638
FOUBabouche <babalefou84@gmail.com>
2025-11-30 10:23:40 +0100

    fix file explorer

--- editor/src/projectFileExplorer.cpp
+++ editor/src/projectFileExplorer.cpp
@@ -37,53 +37,73 @@ void ProjectFileExplorer::update(float dt)
     ImGui::BeginChild("##FileExplorer", ImGui::GetContentRegionAvail(), true);
 
     static bool isMenuOpen = false;
-    static bool onElement = false;
+    static bool isFile = false;
     static std::string triggeredFile = "";
 
     int xCountMax = static_cast<int>(std::floor(ImGui::GetContentRegionAvail().x / 74));
     int i = 1;
-    for(const auto& entry : std::filesystem::directory_iterator(currentFolderPosition)){
-        ImGui::BeginGroup();
-        if(entry.is_directory()){
-            if(ImGui::ImageButton(entry.path().filename().string().c_str(),*folderIcon, {64, 64})){
-                currentFolderPosition = entry.path().string();
-            }
-        }else{
-            ImGui::ImageButton(entry.path().filename().string().c_str(),*fileIcon, {64, 64});
-        }
-        ImGui::Text(entry.path().filename().string().c_str());
-        ImGui::EndGroup();
-        ImVec2 rMin = ImGui::GetItemRectMin();
-        ImVec2 rMax = ImGui::GetItemRectMax();
-        
-        if(ImGui::IsMouseClicked(1)){
-            isMenuOpen = true;
-            onElement = false;
-            if(ImGui::IsMouseHoveringRect(rMin, rMax)){
-                onElement = true;
-                triggeredFile = entry.path().string();
-            }
-        }
-
-        if(xCountMax > 0){
-            if(i % xCountMax != 0){
-                ImGui::SameLine();
+    if(std::filesystem::is_directory(currentFolderPosition)){
+        for(const auto& entry : std::filesystem::directory_iterator(currentFolderPosition)){
+            ImGui::BeginGroup();
+            if(entry.is_directory()){
+                if(ImGui::ImageButton(entry.path().filename().string().c_str(),*folderIcon, {64, 64})){
+                    currentFolderPosition = entry.path().string();
+                    triggeredFile = "";
+                }
             }else{
-                ImGui::NewLine();
+                if(ImGui::ImageButton(entry.path().filename().string().c_str(),*fileIcon, {64, 64})){
+                    std::system(("code "+entry.path().string()).c_str());
+                }
             }
+            ImGui::Text(entry.path().filename().string().c_str());
+            ImGui::EndGroup();
+            ImVec2 rMin = ImGui::GetItemRectMin();
+            ImVec2 rMax = ImGui::GetItemRectMax();
+
+            if(ImGui::IsMouseHoveringRect(rMin, rMax)){
+                if(ImGui::IsMouseClicked(1)){
+                    triggeredFile = entry.path().string();
+                }
+            }
+
+            if(xCountMax > 0){
+                if(i % xCountMax != 0){
+                    ImGui::SameLine();
+                }else{
+                    ImGui::NewLine();
+                }
+            }
+            i++;
         }
-        i++;
+    }else{
+        currentFolderPosition = currentFolderPosition.parent_path();
     }
+    
+    bool onElement = triggeredFile != "";
+
+    if(ImGui::IsMouseClicked(1)){
+        isMenuOpen = true;
+    }
+    
     static bool isFileNameMenuOpen = false;
     if(isMenuOpen){
         if(ImGui::BeginPopupContextWindow()){
-            if(ImGui::MenuItem("Create")){
-                isFileNameMenuOpen = true;
-                isMenuOpen = false;
+            if(ImGui::BeginMenu("Create")){
+                if(ImGui::MenuItem("Folder")){
+                    isFile = false;
+                    isFileNameMenuOpen = true;
+                    isMenuOpen = false;
+                }
+                if(ImGui::MenuItem("File")){
+                    isFile = true;
+                    isFileNameMenuOpen = true;
+                    isMenuOpen = false;
+                }
+                ImGui::EndMenu();
             }
             if(onElement){
                 if(ImGui::MenuItem("Delete")){
-                    std::remove(triggeredFile.c_str());
+                    std::filesystem::remove(triggeredFile.c_str());
                     triggeredFile = "";
                     onElement = false;
                     isMenuOpen = false;
@@ -92,9 +112,6 @@ void ProjectFileExplorer::update(float dt)
             ImGui::EndPopup();
         }
     }
-
-    
-
     ImGui::EndChild();
 
     if(isFileNameMenuOpen){
@@ -102,7 +119,10 @@ void ProjectFileExplorer::update(float dt)
         if(ImGui::BeginPopupModal("File name")){
             char buffer[256] = "";
             if(ImGui::InputText("FileName", buffer, 256, ImGuiInputTextFlags_EnterReturnsTrue)){
-                std::ofstream file(currentFolderPosition.string()+"/"+buffer);
+                if(isFile)
+                    std::ofstream file(currentFolderPosition.string()+"/"+buffer);
+                else
+                    std::filesystem::create_directory((currentFolderPosition.string()+"/"+buffer).c_str());
                 isFileNameMenuOpen = false;
             }
             ImGui::EndPopup();
